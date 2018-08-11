982ccb80db171262edbc15c2b5fbbd3684566b4a
Xi Jin <fate_stigma@hotmail.com>
2018-08-10 17:24:50 -0700

    Change the behavior of parsing graph

--- ontology_viz.py
+++ ontology_viz.py
@@ -15,16 +15,15 @@ colors = namedtuple('Colors', ['cls', 'lit', 'ins'])('#1f77b4', '#ff7f0e', '#e37
 
 
 class OntologyGraph:
-    def __init__(self, files, format='ttl', verbose=False, ontology=None):
+    def __init__(self, files, format='ttl', ontology=None):
         self.g = Graph()
         if ontology is not None:
             g = Graph()
             self._load_files(g, ontology)
+            self.ontology_defined = True
             self.ontology_cls = {cls for cls, in g.query(query_classes)}
-            self.g += g
         else:
-            self.ontology_cls = []
-        self.verbose = verbose
+            self.ontology_defined = False
         self._load_files(self.g, files, format)
         self.classes = set()
         self.instances = set()
@@ -40,26 +39,26 @@ class OntologyGraph:
             graph.load(file, format=format)
 
     def _read_graph(self):
-        for class_, in self.g.query(query_classes):
-            if self.verbose or class_ not in self.ontology_cls:
-                self.classes.add(class_)
-                self._add_predicate_object(class_, True)
-            for instance in self.g.subjects(RDF.type, class_):
-                self.instances.add(instance)
-                self._add_predicate_object(instance)
-
-    def _add_predicate_object(self, subject, is_class=False):
-        for prop, obj in self.g.predicate_objects(subject):
-            if isinstance(obj, Literal):
+        for s, p, o in self.g:
+            if p == RDF.type:
+                if o == OWL.Class:
+                    self.add_to_classes(s)
+                else:
+                    self.add_to_classes(o)
+                    self.instances.add(s)
+                    self.edges.add((s, p, o))
+            elif isinstance(o, Literal):
                 literal_id = uuid4().hex
-                self.literals.add((literal_id, obj))
-                self.edges.add((subject, prop, literal_id))
-            elif self.verbose or obj not in self.ontology_cls:
-                if is_class:
-                    if obj == OWL.Class:
-                        continue
-                    self.classes.add(obj)
-                self.edges.add((subject, prop, obj))
+                self.literals.add((literal_id, o))
+                self.edges.add((s, p, literal_id))
+            else:
+                self.edges.add((s, p, o))
+
+    def add_to_classes(self, cls):
+        if self.ontology_defined:
+            if cls not in self.classes and cls not in self.ontology_cls:
+                print("[WARNING] {} doesn't exist in the ontology!".format(cls))
+        self.classes.add(cls)
 
     def convert(self):
         node_strings = []
@@ -69,7 +68,7 @@ class OntologyGraph:
         for instance in self.instances:
             node_strings.append(self._dot_instance_node(instance))
         for uri, literal in self.literals:
-            node_strings.append('  "{}" [label="{}" shape=rect{}]'.format(uri, literal, node_color(colors.lit)))
+            node_strings.append('  "{}" [label="{}" shape=rect{}]'.format(uri, text_justify(literal, 20), node_color(colors.lit)))
         for s, p, o in self.edges:
             edge_strings.append('  "{}" -> "{}" [label="{}"]'.format(s, o, self._pred_label(p)))
         return node_strings, edge_strings
@@ -125,17 +124,31 @@ def node_color(color):
     return ' fillcolor="{}" color="{}"'.format(color, color)
 
 
+def text_justify(words, max_width):
+    words.replace('"', '\"')
+    words = words.split()
+    res, cur, num_of_letters = [], [], 0
+    max_ = 0
+    for w in words:
+        if num_of_letters + len(w) + len(cur) > max_width:
+            res.append(' '.join(cur))
+            max_ = max(max_, num_of_letters)
+            cur, num_of_letters = [], 0
+        cur.append(w)
+        num_of_letters += len(w)
+    words = res + [' '.join(cur).center(max_)]
+    return '\\n'.join(words)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate dot for the input ontology files')
     parser.add_argument('files', nargs='+', help='Input ontology files.')
     parser.add_argument('-f', '--format', dest='format', default='ttl', help='Input file format.')
     parser.add_argument('-o', '--output', dest='out', default='ontology.dot',
                         help='Location of output dot file.')
-    parser.add_argument('-V', '--verbose', dest='verbose', default=False, action='store_true',
-                        help='Include Classes defined in the ontology.')
     parser.add_argument('-O', '--ontology', dest='ontology', default=None,
                         help='Provided ontology for the graph.')
     args = parser.parse_args()
 
-    og = OntologyGraph(args.files, args.format, verbose=args.verbose, ontology=args.ontology)
+    og = OntologyGraph(args.files, args.format, ontology=args.ontology)
     og.write_file(args.out)
