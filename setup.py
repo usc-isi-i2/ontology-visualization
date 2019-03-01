b003cb14fc66c88b7d0dcc81f1689e8e2c33c0c5
Edmond Chuc <edmond.chuc@outlook.com>
2019-03-02 01:20:30 +1000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,11 @@
 import setuptools
 
-with open("README.org", "r") as fh:
-    long_description = fh.read()
-
-with open('requirements.txt') as req:
-    install_requires = req.read().split("\n")
-
 setuptools.setup(
     name="ontology_visualization",
     version="0.0.1",
     author="fatestigma",
     author_email="fate_stigma@hotmail.com",
     description="",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/edmondchuc/ontology-visualization.git",
-    packages=setuptools.find_packages(),
     license='LICENSE',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=install_requires,
 )
